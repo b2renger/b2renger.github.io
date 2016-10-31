fbdc8e216e184936c5ec7fc968f48296eb02201a
Berenger <berenger.recoules@gmail.com>
2016-10-31 20:17:37 +0100

    new organization building block

--- sketch.js
+++ sketch.js
@@ -1,150 +1,134 @@
 
-// add bounding box for nodes
-// feature : add sound ! and button to mute it :)
-
-// correct search bar
-// beeter layout
-// latest p5js version ?
-
 
 
 var nodes = [];
 var springs = [];
-
 var tags = [];
 
-var longdistance = 120 ;
-var shortdistance = 45;
+var longdistance = 200 ;
+var shortdistance = 100;
 
 var selectedNode =null;
 var selectedNodeX;
 var selectedNodeY;
 
-
-var cssTitle = "font-family: 'Open Sans Condensed', sans-serif; background-color:#000000; color:#FFFFFF; font-size:18pt; padding:10px;text-align: center;";
-var cssButton = "font-family: 'Open Sans Condensed', sans-serif; background-color:#000000; color:#FFFFFF; font-size:17pt; padding:2px;text-align: center;";
+var cssTitle = "font-family: 'Open Sans Condensed', sans-serif; background-color:#000000; color:#FFFFFF; font-size:24pt; padding:10px;text-align: center;";
+var cssButton = "font-family: 'Open Sans Condensed', sans-serif; background-color:#000000; color:#FFFFFF; font-size:20pt; padding:2px;text-align: center;";
 var cssTags = "font-family: 'Open Sans Condensed', sans-serif; background-color:#000000; color:#FFFFFF; padding:10px;";
-var cssParagraph = "font-family:monospace; background-color:#000000; color:#FFFFFF; padding:10px;";
-var cssLink = "font-family: 'Open Sans Condensed', sans-serif;; background-color:#000000; color:#FFFFFF; font-size:10pt; padding:10px;";
+var cssParagraph = "font-family:monospace; background-color:#000000; color:#FFFFFF; padding:10px;font-size:12pt;";
+var cssLink = "font-family: 'Open Sans Condensed', sans-serif;; background-color:#000000; color:#FFFFFF; font-size:12pt; padding:10px;";
 
 var t = 0; // for pulsing nodes
 
 var input, button;
 
+var teach = 1
+var com = 2
+var perso = 3
+
+var splitfactor = 2;
+
 
 function init(){
-
   setPage(nodes[0].page);
 }
 
 function setup() {
   	
-  createCanvas(windowWidth/3  , 1080);
+  createCanvas(windowWidth/splitfactor  , 1080);
   smooth();
 
   textFont("Open Sans Condensed");
+  textSize(14);
 
   nodes = [];
   springs = [];
+
   //0
-  nodes.push(new Node((width*2/5),(height/12),'HOME',true));
+  nodes.push(new Node((width*4/5),(height/12),'HOME',true));
   nodes[0].setProject("pages/home.csv");
 
   //1
-  nodes.push(new Node(width*3/5,height/4,'WEB',true));
-  addConnection(0,200);
-  nodes[1].setProject("pages/Web.csv");
-   //2
-  nodes.push(new Node(width*3/5,height*2/4,'WEBPD',true));
-  addConnection(1,150);
-  nodes[2].setProject("pages/Web.csv");
+  nodes.push(new Node((width*3/5),(height*3/4),'Teaching and ressources',true));
+  nodes[1].setProject("pages/home.csv");
+
+  //2
+  nodes.push(new Node(width*1/10,height*1/4,'Commissionned work',true));
+  nodes[2].setProject("pages/home.csv");
+  
   //3
-  nodes.push(new Node(width*4/5,height/4,'P5JS',true));
-  addConnection(1,150);
-  nodes[3].setProject("pages/Web.csv");
-  //4
-  nodes.push(new Node(width*2/5,height/4,'ANDROID',true));
-  addConnection(0,200);
-  nodes[4].setProject("pages/Android.csv");
-  //5
-  nodes.push(new Node(width*1/6,random(height/8,height/3),'DESKTOP',true));
-  addConnection(0,200);
-  nodes[5].setProject("pages/Desktop.csv");
+  nodes.push(new Node(width*3/10,height*2/4,'Personal work',true));
+  nodes[3].setProject("pages/home.csv");
+  
 
   // android
-  nodes.push(new Node((nodes[4].location.x),(nodes[4].location.y+20),'DroidParty tutorials',false));
-  addConnection(4,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'DroidParty tutorials',false));
+  addConnection(teach,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/droidparty_tuto.csv");
 
-  nodes.push(new Node((nodes[4].location.x),(nodes[4].location.y+20),'Springs',false));
-  addConnection(4,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'Springs',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/springs.csv");
 
-  nodes.push(new Node((nodes[4].location.x),(nodes[4].location.y+20),'Musicbox 3D',false));
-  addConnection(4,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x)+random(-20,20),(nodes[perso].location.y+random(-20,20)),'Musicbox 3D',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/musicbox3d.csv");  
 
-  nodes.push(new Node((nodes[4].location.x),(nodes[4].location.y+20),'Pendulum Phase',false));
-  addConnection(4,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x)+random(-20,20),(nodes[perso].location.y+random(-20,20)),'Pendulum Phase',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/pendulum-phase.csv");  
 
-    nodes.push(new Node((nodes[4].location.x),(nodes[4].location.y+20),'PPP',false));
-  addConnection(4,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'PPP',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/ppp.csv");  
 
   // desktop
-  nodes.push(new Node((nodes[5].location.x),(nodes[5].location.y+20),'Data Sonification',false));
-  addConnection(5,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'Data Sonification',false));
+  addConnection(teach,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/datasonification.csv");  
 
-  nodes.push(new Node((nodes[5].location.x),(nodes[5].location.y+20),'Blends and shaders',false));
-  addConnection(5,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'Blends and shaders',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/blends_n_shaders.csv");  
 
-  nodes.push(new Node((nodes[5].location.x),(nodes[5].location.y+20),'Pure-Data Intro',false));
-  addConnection(5,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'Pure-Data Intro',false));
+  addConnection(teach,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/cours-pd.csv");  
 
-  nodes.push(new Node((nodes[5].location.x),(nodes[5].location.y+20),'Processing Intro',false));
-  addConnection(5,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'Processing Intro',false));
+  addConnection(teach,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/cours-processing.csv");  
 
-
-
   // web
- 
-  nodes.push(new Node((nodes[3].location.x),(nodes[3].location.y+20),'Happy Birthday codelab',false));
-  addConnection(3,random(shortdistance,longdistance));
-  addConnection(2,random(shortdistance,longdistance));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'Happy Birthday codelab',false));
+  addConnection(perso,random(shortdistance,longdistance));
   nodes[nodes.length-1].setProject("pages/codelab_sonification.csv");  
 
-  nodes.push(new Node((nodes[2].location.x),(nodes[2].location.y+20),'Webpd getting started',false));
-  addConnection(2,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'Webpd getting started',false));
+  addConnection(teach,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/Webpd_getting_started.csv"); 
 
-  nodes.push(new Node((nodes[2].location.x),(nodes[2].location.y+20),'Webpd experiments',false));
-  addConnection(2,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'Webpd experiments',false));
+  addConnection(perso,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/Webpd_experiments.csv");    
 
-  nodes.push(new Node((nodes[2].location.x),(nodes[2].location.y+20),'The midst ...',false));
-  addConnection(2,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[com].location.x+random(-20,20)),(nodes[com].location.y+random(-20,20)),'The midst ...',false));
+  addConnection(com,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/the_midst.csv");  
 
-  nodes.push(new Node((nodes[2].location.x),(nodes[2].location.y+20),'Rennes soundscape',false));
-  addConnection(2,random(shortdistance,shortdistance+shortdistance/2));
+  nodes.push(new Node((nodes[com].location.x+ random(-20,20)),(nodes[com].location.y+random(-20,20)),'Rennes soundscape',false));
+  addConnection(com,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/Electroni-k.csv");  
 
-
-  nodes.push(new Node((nodes[3].location.x)+20,(nodes[3].location.y-50),'this website',false));
-  addConnection(3,230);
-  addConnection(0,230);
+  nodes.push(new Node((nodes[0].location.x),(nodes[0].location.y+50),'this website',false));
+  addConnection(0,75);
   nodes[nodes.length-1].setProject("pages/algae-DOM.csv");  
 
-  nodes.push(new Node((nodes[3].location.x),(nodes[3].location.y+20),'P5js sound experiments',false));
+  nodes.push(new Node((nodes[perso].location.x+random(-20,20)),(nodes[perso].location.y+random(-20,20)),'P5js sound experiments',false));
   addConnection(3,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/p5js_sound_experiments.csv");  
 
-  nodes.push(new Node((nodes[1].location.x),(nodes[1].location.y-40),'Openprocessing - Reinetiere',false));
+  nodes.push(new Node((nodes[teach].location.x+random(-20,20)),(nodes[teach].location.y+random(-20,20)),'Openprocessing - Reinetiere',false));
   addConnection(1,random(shortdistance,shortdistance+shortdistance/2));
   nodes[nodes.length-1].setProject("pages/reinetiere.csv");  
   
@@ -189,7 +173,7 @@ function mouseDragged(){
 
 function mouseMoved() {
   var maxDist = 10; 
-  if(mouseX<590){ // stay in our canvas range
+  if(mouseX<windowWidth/2){ // stay in our canvas range
     for (var i = 0; i < nodes.length; i++) {
       var d = dist(mouseX, mouseY, nodes[i].location.x, nodes[i].location.y);
       if (d < maxDist && nodes[i].overMe == false) {      
@@ -225,7 +209,7 @@ function setPage(project){
   var type ;
   var linebreak;
   var posY = -20; // keep track of where we're at building the page
-  var posX = 600;
+  var posX = windowWidth/splitfactor;
 
   for (var i = 1 ; i < project.getRowCount(); i++){
     row = project.getRow(i);
@@ -240,7 +224,7 @@ function setPage(project){
 
       if (linebreak == 'True' || linebreak == ' True'){
         posY += title.height  ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += title.width;
       }
@@ -254,7 +238,7 @@ function setPage(project){
 
       if (linebreak == 'True' || linebreak == ' True'){
         posY += vid.height ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += vid.width;
       }
@@ -268,7 +252,7 @@ function setPage(project){
 
       if (linebreak == 'True' || linebreak == ' True'){
         posY += description.height ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += description.width;
       }
@@ -277,12 +261,12 @@ function setPage(project){
     else if (type == 'Image'){
       var image = createImg(str);
       image.style(cssParagraph);
-      image.size(350,200);
+      image.size(AUTO,300);
       image.position(posX,posY);
 
       if (linebreak == 'True' || linebreak == ' True'){
         posY += image.height;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += image.width;
       }
@@ -298,7 +282,7 @@ function setPage(project){
 
       if (linebreak == 'True' || linebreak == ' True'){
         posY += p.height ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += 150;
       }
@@ -311,7 +295,7 @@ function setPage(project){
 
        if (linebreak == 'True' || linebreak == ' True'){
         posY += ic.height  ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else{
         posX += ic.width;
       }
@@ -327,7 +311,7 @@ function setPage(project){
 
       if (linebreak == 'True'|| linebreak == ' True'){
         posY += link.height  ;
-        posX = 600;
+        posX = windowWidth/splitfactor;
       } else {
         posX += link.width;
       }
@@ -340,7 +324,6 @@ function setPage(project){
 function check(param){
   console.log("over " + param.currentTarget.innerHTML);
  
-
   for (var i = 0 ; i < nodes.length ; i++){
     var checkpage = nodes[i].page;
     nodes[i].highlight = false;
